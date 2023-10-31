117aa82a64b2343540524a811342bf36f13cbd0c
tanto <tantows001@gmail.com>
2023-10-31 10:42:02 +0700

    final commit

--- app/src/main/java/com/example/mymovie/ProfileFragment.kt
+++ app/src/main/java/com/example/mymovie/ProfileFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +34,15 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val username = arguments?.getString("usn")
+
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        val tvUsername = view.findViewById<TextView>(R.id.tvUsername)
+
+        // Now, set the text of the TextView to the 'username' variable
+        tvUsername.text = username
+
+        return view
     }
 
     companion object {
